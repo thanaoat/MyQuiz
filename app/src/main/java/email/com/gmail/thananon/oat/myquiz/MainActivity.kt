f081a9eaad1d652a887ec1c713eacd280983cd7d
Thananon Kumkrong <thananon.oat@gmail.com>
2020-01-10 10:01:26 +0700

    Init

---
+++ app/src/main/java/email/com/gmail/thananon/oat/myquiz/MainActivity.kt
@@ -0,0 +1,12 @@
+package email.com.gmail.thananon.oat.myquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
