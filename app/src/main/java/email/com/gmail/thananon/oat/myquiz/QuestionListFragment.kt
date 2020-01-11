e5c9af7fe7eb84a80556e9eaad4d2e2a12dfc8f7
Thananon Kumkrong <thananon.oat@gmail.com>
2020-01-11 22:08:47 +0700

    Add QuestionAdapter and QuestionHolder

--- app/src/main/java/email/com/gmail/thananon/oat/myquiz/QuestionListFragment.kt
+++ app/src/main/java/email/com/gmail/thananon/oat/myquiz/QuestionListFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import email.com.gmail.thananon.oat.myquiz.database.Question
 
 class QuestionListFragment : Fragment() {
 
     private lateinit var questionRecyclerView: RecyclerView
+    private var adapter: QuestionAdapter = QuestionAdapter(emptyList())
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +24,38 @@ class QuestionListFragment : Fragment() {
 
         questionRecyclerView = view.findViewById(R.id.questionRecyclerView)
         questionRecyclerView.layoutManager = LinearLayoutManager(context)
+        questionRecyclerView.adapter = adapter
 
         return view
+    }
+
+    private inner class QuestionHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+        private lateinit var question: Question
+
+        val questionText: TextView = itemView.findViewById(R.id.questionText)
+
+        fun bind(question: Question) {
+            this.question = question
+            questionText.text = question.text
+        }
+    }
+
+    private inner class QuestionAdapter(var questions: List<Question>)
+        : RecyclerView.Adapter<QuestionHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_question, parent, false)
+            return QuestionHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return questions.size
+        }
+
+        override fun onBindViewHolder(holder: QuestionHolder, position: Int) {
+            val question = questions[position]
+            holder.bind(question)
+        }
     }
 }
